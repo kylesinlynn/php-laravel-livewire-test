6a221eca32aa759bc3edbce16d4e8f4a569d0649
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-04-10 15:48:51 +0630

    created comment model and livewire setup

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
+    public $comments = [
+        [
+            'content' => 'First comment',
+            'creator' => 'Daniel',
+            'created_at' => '2020-01-01 12:00:00',
+        ]
+    ];
+
+    public $newComment;
+
+    public function addComment()
+    {
+        array_unshift($this->comments, [
+            'content' => $this->newComment,
+            'creator' => 'Daniel',
+            'created_at' => Carbon::now()->diffForHumans(),
+        ]);
+
+        $this->newComment = '';
+    }
+
     public function render()
     {
         return view('livewire.comments');
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,3 +1,12 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <form wire:submit.prevent='addComment'>
+        <input type="text" wire:model.lazy='newComment'>
+        <button type="submit">Add Comment</button>
+    </form>
+
+    @foreach ($comments as $comment)
+        <div>
+            {{ $comment['creator'] }}: {{ $comment['content'] }} [ {{ $comment['created_at'] }} ]
+        </div>
+    @endforeach
 </div>
